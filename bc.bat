910a79c3dabb44179ed5562dd8edbc6c22cbd6f5
Peter Lanoie <peter.lanoie@octopus.com>
2020-11-25 10:47:46 -0500

    Make it launch BC4 OR BC3

--- bc.bat
+++ bc.bat
@@ -1,1 +1,11 @@
-@start "Beyond Compare" "C:\Program Files (x86)\Beyond Compare 3\bcompare.exe" %*
+@echo off
+if exist "c:\Program Files\Beyond Compare 4\BCompare.exe" (
+	start "Beyond Compare 4" "c:\Program Files\Beyond Compare 4\BCompare.exe" %*
+	goto end
+)
+if exist "C:\Program Files (x86)\Beyond Compare 3\bcompare.exe" (
+	start "Beyond Compare 3" "C:\Program Files (x86)\Beyond Compare 3\bcompare.exe" %*
+	goto end
+)
+
+:end 
